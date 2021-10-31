fb363664a3538b6a8c7c130f3beb1c1eea69534a
thousand10 <aurelianrobot@gmail.com>
2021-10-30 20:32:08 -0700

    making the website responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const btn = document.getElementById("btn")
+const curten = document.getElementById("curten")
+btn.addEventListener("click", () => {
+    curten.classList.toggle("hidden")
+})
